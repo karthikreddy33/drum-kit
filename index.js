8861430ea38fc3ce531d2182c3da47eb176037c4
karthikreddy33 <165651552+karthikreddy33@users.noreply.github.com>
2024-04-01 14:46:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+
+var number = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < number; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        var keyWords = this.innerHTML;
+        makeSound(keyWords);
+        makeAnimations(keyWords);
+    });
+}
+
+addEventListener("keydown", function (event) {
+    makeSound(event.key);
+    makeAnimations(event.key);
+
+});
+
+function makeSound(key) {
+    switch (key) { // Changed keyWords to key
+        case "w":
+            var audio = new Audio('./sounds/tom-1.mp3');
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio('./sounds/tom-2.mp3');
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio('./sounds/tom-3.mp3');
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio('./sounds/tom-4.mp3');
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio('./sounds/snare.mp3');
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio('./sounds/kick-bass.mp3');
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio('./sounds/crash.mp3');
+            audio.play();
+            break;
+    }
+}
+function makeAnimations(currentKey){
+ var activeButton= document.querySelector("."+currentKey);
+ 
+ activeButton.classList.add("pressed");
+
+
+ setTimeout( function(){
+    activeButton.classList.remove("pressed")
+ },100);
+}
